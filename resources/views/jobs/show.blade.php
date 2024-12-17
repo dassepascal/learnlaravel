0f2c5b0e9092d2df546f1b3052bed7489206aa0a
Dasse <pascaldasse56@gmail.com>
2024-12-17 15:35:01 +0100

    edit

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,10 +3,16 @@
        Job 
     </x-slot:heading>
 
-    <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{$job['title']}}</h1>
+    <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{$job->title}}</h1>
 
     <p>
-        This job pays {{$job['salary']}} per year.
+        This job pays {{$job->salary}} per year.
+    </p>
+
+    <p class="mt-6 text-base leading-7 text-gray-600">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+
+
     </p>
 
     
