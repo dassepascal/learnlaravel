d78930d520e114d9416a7dbe3648fea78598ec29
Dasse <pascaldasse56@gmail.com>
2025-01-08 09:34:17 +0100

    modif register

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -7,12 +7,13 @@ use Illuminate\Http\Request;
 class RegisteredUserController extends Controller
 {
     public function create(){
+        
      
         return view('auth.register');
     }
     public function  store(){
 
-        dd(request()->all());
+       // dd(request()->all());
        
     }
 }
